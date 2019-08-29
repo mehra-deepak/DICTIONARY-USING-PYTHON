af5200dddbf01c77f74329134c7f57ce52e54e73
mehra-deepak <mehradeepak0608@gmail.com>
2019-08-29 18:32:00 +0530

    Dictionary in Python

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-Dictionary in python
+# Dictionary in python
+
+Dict = {1: 'Aplles' 2: 'are', 3: 'red'} 
+print("\nDictionary with the use of Integer Keys: ") 
+print(Dict) 
+
+# The Numbers are the Key and The string are values.
+# So if a key is passed or traversed along the Dictionary the value associated with it is 
+# is printe as well.
+
+a=int(input("enter the number:"))
+
+if a in Dict:
+    print(Dict[a])
